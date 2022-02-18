0dae7b93959a89463933714ac1b12a1c25e2de11
coderbhavya24 <bhagrawal24@gmail.com>
2022-02-19 01:46:03 +0530

    working on vdo

--- lib/reel.dart
+++ lib/reel.dart
@@ -2,8 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:myclone/search.dart';
 import 'package:myclone/activity.dart';
 import 'package:myclone/page.dart';
+import 'package:video_player/video_player.dart';
 
 class reels extends StatelessWidget {
+  late VideoPlayerController _controller;
+  @override
+  void initState() {
+    super.initState();
+    // Pointing the video controller to our local asset.
+    _controller = VideoPlayerController.asset("images/new_vedio.mp4")
+      ..initialize().then((_) {
+        // Once the video has been loaded we play the video and set looping to true.
+        _controller.play();
+        _controller.setLooping(true);
+        // Ensure the first frame is shown after the video is initialized.
+        setState(() {});
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
